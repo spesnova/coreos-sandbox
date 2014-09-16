0d18bf5c219f9ad210b8a9ea7d63053506e6e035
Seigo Uchida <spesnova@gmail.com>
2014-09-16 17:14:22 +0900

    Add test for registrator.service

--- spec/core-01/coreos_spec.rb
+++ spec/core-01/coreos_spec.rb
@@ -11,9 +11,17 @@ end
 #
 # Checks for journal-router
 #
+describe file("/etc/systemd/system/journal-router-short.service") do
+  it { should be_file }
+end
+
 describe command("systemctl status journal-router-short.service") do
   its(:stdout) { should match /active \(running\)/ }
   its(:exit_status) { should eq 0 }
+end
+
+describe file("/etc/systemd/system/journal-router-json.service") do
+  it { should be_file }
 end
 
 describe command("systemctl status journal-router-json.service") do
@@ -72,6 +80,10 @@ end
 #
 # Checks for registry.service
 #
+describe file("/etc/systemd/system/registry.service") do
+  it { should be_file }
+end
+
 describe command("systemctl status registry.service") do
   its(:stdout) { should match /active \(running\)/ }
   its(:exit_status) { should eq 0 }
@@ -85,6 +97,10 @@ end
 #
 # Checks for logspout.service
 #
+describe file("/etc/systemd/system/logspout.service") do
+  it { should be_file }
+end
+
 describe command("systemctl status logspout.service") do
   its(:stdout) { should match /active \(running\)/ }
   its(:exit_status) { should eq 0 }
@@ -103,6 +119,10 @@ end
 #
 # Checks for dd-agent.service
 #
+describe file("/etc/systemd/system/dd-agent.service") do
+  it { should be_file }
+end
+
 describe command("systemctl status dd-agent.service") do
   its(:stdout) { should match /active \(running\)/ }
   its(:exit_status) { should eq 0 }
@@ -111,6 +131,41 @@ end
 describe command("docker ps | grep dd-agent") do
   its(:stdout) { should match /Up/ }
   its(:exit_status) { should eq 0 }
+end
+
+#
+# Checks for registrator.service
+#
+describe file("/etc/systemd/system/registrator.service") do
+  it { should be_file }
+end
+
+describe command("systemctl status registrator.service") do
+  its(:stdout) { should match /active \(running\)/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("docker ps | grep registrator") do
+  its(:stdout) { should match /Up/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("etcdctl ls services/registry") do
+  its(:stdout) { should match /\/services\/registry\/core-01:registry:5000/ }
+  its(:stdout) { should match /\/services\/registry\/core-02:registry:5000/ }
+  its(:stdout) { should match /\/services\/registry\/core-03:registry:5000/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("etcdctl ls services/logspout") do
+  its(:stdout) { should match /\/services\/logspout\/core-01:logspout:8000/ }
+  its(:stdout) { should match /\/services\/logspout\/core-02:logspout:8000/ }
+  its(:stdout) { should match /\/services\/logspout\/core-03:logspout:8000/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("curl -I localhost:4001/v2/keys/services/registry/core-01:registry:5000") do
+  its(:stdout) { should match /200/ }
 end
 
 #
